5ad9601a89cf4e5981e883876be664a9b52e5ff7
Muhammad Sharjeel <03-135232-059@student.bahria.edu.pk>
2025-10-23 18:37:28 +0500

    add some new lines of code

--- lib/Pages/page2.dart
+++ lib/Pages/page2.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant/resource/picture.dart';
 import 'package:restaurant/resource/colourScheme.dart';
+import 'package:restaurant/resource/appFonts.dart';
 
 
 class homePage2 extends StatefulWidget {
@@ -13,12 +14,32 @@ class homePage2 extends StatefulWidget {
 class _homePage2State extends State<homePage2> {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: AppBar(
         backgroundColor: colors.red,
         elevation: 0,
 
+        leading: IconButton(onPressed: (){},
+            icon: Icon(Icons.menu , size: 35, color: Colors.white,)),
+
+
+        // Add Location Logic here
+        title: Text("Welcome", style: TextStyle(
+          fontFamily: fonts.font1,
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),),
+
+        //Add search filter / search window here
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 15),
+            child: Icon(Icons.search, size: 35, color: Colors.white,),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/resource/appFonts.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class fonts{
+  static const String font1 = "Times New Roman";
+}
